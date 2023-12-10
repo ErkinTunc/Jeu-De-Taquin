14239e1b4aed35e3a5da2ec9c9a0e289c6f3b010
ErkinTunc <77968721+ErkinTunc@users.noreply.github.com>
2023-12-10 21:06:28 +0100

    ajout de fonction (creer le table)

--- Code_Taquin.c
+++ Code_Taquin.c
@@ -1,18 +1,24 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+// === Fonction Pour Tester
 
 
+// === Fonction De Jeu ===
 
-
-
-
-
+double ** CreationTab(){
+	//Il cree une table 4x4 
+	int **tab = malloc(4*sizeof(int));
+	int i;
+	for(i=0;i<4;i++){
+		tab[i] = malloc(4*sizeof(int));
+	}
+	
+	return tab;
+}
 
 
 int main(){
-
-
 
 
 return 0;
